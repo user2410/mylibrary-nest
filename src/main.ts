499dcc94e3a1a024c113bd661d59aad53741f174
user2410 <dc241024@gmail.com>
2023-03-22 09:53:34 +0700

    first commit

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { VersioningType } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.setGlobalPrefix('api');
+  app.enableCors();
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('My library API')
+    .setDescription('My Library API documentation')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const configs = app.get(ConfigService);
+  console.log(configs);
+
+  const port: number = configs.get('PORT') as number;
+  await app.listen(port, () => console.log(`Listening on port ${port}`));
+}
+bootstrap();
